7c31902206d1553e5cb9a3a3d7b0b8e524e41487
Snehal0301 <snehal@robosoftin.com>
2022-11-15 15:15:17 +0530

    First Commit

--- src/index.js
+++ src/index.js
@@ -6,10 +6,36 @@ import { configureStore } from '@reduxjs/toolkit'
 import { Provider } from 'react-redux'
 import detailReducer from './slice/detailSlice';
 
+const localStorageMiddleware = ({ getState }) => {
+
+  return (next) => (action) => {
+
+    const result = next(action);
+
+    localStorage.setItem("applicationState", JSON.stringify(getState()));
+
+    return result;
+
+  };
+
+};
+
+const reHydrateStore = () => {
+
+  if (localStorage.getItem("applicationState") !== null) {
+
+    return JSON.parse(localStorage.getItem("applicationState")); // re-hydrate the store
+
+  }
+
+};
 const store = configureStore({
   reducer: {
     users: detailReducer
-  }
+  },
+  preloadedState: reHydrateStore(),
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware().concat(localStorageMiddleware),
 })
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
